69edd647a5bb43d51de778951ac48b4fb6935b76
Deveshshukla4 <deveshshukla4@gmail.com>
2017-04-13 11:19:43 +0530

    first commit

---
+++ insta_bot.py
@@ -0,0 +1,6 @@
+import requests # Requests library imported to perform different queries such as get , post , delete ,put
+
+
+APP_ACCESS_TOKEN = "3068983250.94b2134.006490b178c24fefa74dba819dddaa1c" # access token
+
+BASE_URL = "https://api.instagram.com/v1/"
