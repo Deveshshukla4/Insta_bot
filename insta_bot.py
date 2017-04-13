ef8d027eae2fc0c672d103c536a555d7ccbe2d10
Deveshshukla4 <deveshshukla4@gmail.com>
2017-04-13 11:20:23 +0530

    Update insta_bot.py

--- insta_bot.py
+++ insta_bot.py
@@ -4,3 +4,19 @@ import requests # Requests library imported to perform different queries such as
 APP_ACCESS_TOKEN = "3068983250.94b2134.006490b178c24fefa74dba819dddaa1c" # access token
 
 BASE_URL = "https://api.instagram.com/v1/"
+
+#Function to prints the user info
+
+def self_info():
+    requests_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN) #req to fetech the user info
+    my_info = requests.get(requests_url).json() #response from api
+
+
+    print "Name :" + str(my_info['data']['full_name'])
+    print "Bio :" + str(my_info['data']['bio'])
+    print "Followers :" + str(my_info['data']['counts']['followed_by'])
+
+    get_user_by_username()#calling function
+
+#function to search the username and perform operations
+
